791ff22361d873a2e2af0108dcc2aabcecea8588
IlmarsKozlovs <ilmars.kozlovs@inbox.lv>
2018-02-27 21:48:11 +0200

    Request/Response model

--- src/main/java/TaskApplication.java
+++ src/main/java/TaskApplication.java
@@ -1,44 +1,50 @@
-import domain.Task;
+import businesslogic.CreateTaskService;
+import businesslogic.RemoveAllTasksService;
+import businesslogic.RemoveTaskService;
+import businesslogic.TaskListService;
+import businesslogic.impl.CreateTaskServiceImpl;
+import businesslogic.impl.RemoveAllTasksServiceImpl;
+import businesslogic.impl.RemoveTaskServiceImpl;
+import businesslogic.impl.TaskListServiceImpl;
+import businesslogic.userinterface.*;
+import database.Database;
+import database.InMemoryDatabase;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class TaskApplication {
 
     public static void main(String[] args) {
 
-        List<Task> tasks = new ArrayList<>();
-        while (true){
+        Database database = new InMemoryDatabase();
+
+        CreateTaskService createTaskService = new CreateTaskServiceImpl(database);
+        RemoveTaskService removeTaskService = new RemoveTaskServiceImpl(database);
+        RemoveAllTasksService removeAllTasksService = new RemoveAllTasksServiceImpl(database);
+        TaskListService taskListService = new TaskListServiceImpl(database);
+
+        Map<Integer, View> commands = new HashMap<>();
+        commands.put(1, new CreateTaskView(createTaskService));
+        commands.put(2, new RemoveTaskView(removeTaskService));
+        commands.put(3, new RemoveAllTasksView(removeAllTasksService));
+        commands.put(4, new PrintTaskListView(taskListService));
+
+
+        while (true) {
             printApplicationMenu();
             int menuItem = getFromUserApplicationItemToExecute();
-            if (menuItem == 5){
+            if (menuItem == 5) {
                 break;
             }
-            switch (menuItem){
-                case 1: {
-                    createNewTask(tasks);
-                    break;
-                }
-                case 2: {
-                    removeTask(tasks);
-                    break;
-                }
-                case 3:{
-                    removeAllTasks(tasks);
-                    break;
-                }
-                case 4:{
-                    printTaskList(tasks);
-                    break;
-                }
-            }
+            View view = commands.get(menuItem);
+            view.execute();
         }
-
     }
 
-    private static void printApplicationMenu(){
+
+    private static void printApplicationMenu() {
         System.out.println("Application Menu: ");
         System.out.println("1. Create new task");
         System.out.println("2. Remove task from list");
@@ -47,62 +53,9 @@ public class TaskApplication {
         System.out.println("5. Exit application");
     }
 
-    private static int getFromUserApplicationItemToExecute(){
+    private static int getFromUserApplicationItemToExecute() {
         System.out.println("Please enter application item number to execute:");
         Scanner scanner = new Scanner(System.in);
         return Integer.parseInt(scanner.nextLine());
-    }
-
-    private static void createNewTask(List<Task> tasks){
-        System.out.println();
-        System.out.println("Create new Task !");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Task name: ");
-        String name = sc.nextLine();
-        System.out.println("Enter Task description: ");
-        String description = sc.nextLine();
-        Task task = new Task();
-        task.setName(name);
-        task.setDescription(description);
-        tasks.add(task);
-        System.out.println("Create new Task execution end!");
-        System.out.println();
-    }
-
-    private static void removeTask(List<Task> tasks){
-        System.out.println();
-        System.out.println("Remove existing Task !");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Task to delete");
-        final String name = sc.nextLine();
-        Optional<Task> foundTask = tasks.stream()
-                .filter(p -> p.getName().equals(name))
-                .findFirst();
-        if (foundTask.isPresent()){
-            System.out.println("Task with name " + name + "found, and will be removed from list!");
-            Task task = foundTask.get();
-            tasks.remove(task);
-        }
-        else {
-            System.out.println("Task with name " + name + "was not found, and will not be removed from list!");
-        }
-        System.out.println("Remove existing task execution end!");
-    }
-
-    private static void removeAllTasks(List<Task> tasks){
-        System.out.println();
-        System.out.println("Remove all tasks !");
-        tasks.clear();
-        System.out.println("Remove all tasks execution end!");
-        System.out.println();
-    }
-    private static void printTaskList(List<Task> tasks){
-        System.out.println();
-        System.out.println("Print task list to console !");
-        for (Task t: tasks) {
-            System.out.println(t.getName() + t.getDescription() + "]");
-        }
-        System.out.println("Print task list to console execution end!");
-        System.out.println();
     }
 }
